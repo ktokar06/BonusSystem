321049ba5e3b92ac4bf7b3721e44134a9cdb464d
Astf1x <ivannechaev07@gmail.com>
2024-11-08 13:16:43 +0300

    Transaction index, store

--- app/Http/Controllers/Api/TransactionController.php
+++ app/Http/Controllers/Api/TransactionController.php
@@ -15,25 +15,111 @@ class TransactionController extends Controller
     {
         $limit_number = $request->header('limit');
         $userId       = $request->header('userId');
+
+        $allTransactions = Transaction::all();
+
+        $sortedTransactions = [];
+        foreach ($allTransactions as $transaction) {
+            if (isset($transaction['senderId']) && $transaction['senderId'] == $userId) {
+                $sortedTransactions[] = $transaction;
+            }
+        }
+
+        $countTransactions = count($sortedTransactions);
         
-        $totalTransactions = Transaction::count();
-        
-        if ($limit_number >= $totalTransactions) {
-			return response()->json(Transaction::all());
+        if ($limit_number >= $countTransactions) {
+			return response()->json(sortedTransactions);
         }
 
         $limit = $request->input('limit', $limit_number);
-            
-        $transactions = Transaction::limit($limit)->get();
+        
+        $result = array_slice($sortedTransactions, 0, $countTransactions + 1);
 
-		return response()->json($transactions);
+		return response()->json($result); // Incorrect working
     }
 
 
     public function store(StoreTransactionRequest $request)
-    {			
-		//return $request;	
-		//return Transaction::create($request::all());
+    {
+       /*
+        * Создает транзакцию между пользователями
+        * для передачи денег
+        */ 
+        
+        $data = $request->all();
+
+        $senderId    = $data['senderId'   ];        
+        $recipientId = $data['recipientId'];
+
+        if ($senderId == $recipientId) {
+            return response('Invalid recipientId', 422)
+                ->header('Content-type', 'text/plain');         
+        }
+
+        $value       = $data['value'];
+
+        $senderValue    = $this->getBalance($senderId,    'rub'); 
+        $recipientValue = $this->getBalance($recipientId, 'rub'); 
+
+        if ($senderValue < 0) { 
+            return response("Sender User not have balance", 422)
+                ->header('Content-type', 'text/plain'); 
+        } else if ($recipientValue < 0) {   
+            return response("Recipient User not have balance", 422)
+                ->header('Content-type', 'text/plain'); 
+        }
+
+        if ($senderValue - $value < 0) {
+            return response('Not enough value', 422)
+                ->header('Content-type', 'text/plain');     
+        }
+        
+        $senderValue    -= $value;
+        $recipientValue += $value;  
+
+        DB::statement("UPDATE user_balance SET value = $senderValue
+                       WHERE  \"accountId\"='$senderId'");      
+        DB::statement("UPDATE user_balance SET value = $recipientValue
+                       WHERE  \"accountId\"='$recipientId'");       
+
+        do { 
+            $word = array_merge(range('a', 'z'), range('A', 'Z'));
+            shuffle($word);
+            $transactionId = substr(implode($word), 0, 16);
+
+        } while (DB::select("SELECT * FROM transaction
+                             WHERE \"transactionId\" = '$transactionId'")); 
+
+        $query = "INSERT INTO transaction (\"transactionId\",
+                                           \"senderId\",
+                                           \"recipientId\",
+                                           \"currencyType\",
+                                             value)
+                  values (?, ?, ?, ?, ?)";
+        
+        DB::insert($query, [$transactionId, $senderId, $recipientId, 'rub', $value]); 
+                    
+        return response('Success', 200)
+            ->header('Content-type', 'text/plain'); 
+    }
+
+    public function getBalance($accountId, $type) {
+        
+        $balanceQuery = "SELECT * FROM user_balance
+                         WHERE \"accountId\" = '$accountId'
+                         AND
+                         \"balanceType\"='$type'";      
+
+        $balanceInfo = DB::select($balanceQuery);
+        
+        if (!$balanceInfo) {
+            return -1;
+        }       
+
+        $balanceArray = json_decode(json_encode($balanceInfo), true);
+        $value        = $balanceArray[0]['value'];      
+        
+        return $value;              
     }
 
 
